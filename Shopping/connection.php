34e27b7a1836510ffd4868c9b825f126b4598d8e
lekhavan <91652143+lekhavan@users.noreply.github.com>
2021-10-23 08:52:19 +0700

    .

--- Shopping/connection.php
+++ Shopping/connection.php
@@ -1,7 +1,7 @@
 <?php
-    $conn = mysqli_connect('localhost', 'root', '', '1111') or die("Can not connect database".mysqli_connect_error());
-    /*$Connect = pg_connect("postgres://kkhbsaqkzdqykx:54ddd61a4283d1770df62432ed7f08b0ac42ba7ae7d9e2bb519810225fff3af3@ec2-3-212-168-103.compute-1.amazonaws.com:5432/df5bjm5q08o5k2");
+        //$conn = mysqli_connect('localhost', 'root', '', '1111') or die("Can not connect database".mysqli_connect_error());
+    $Connect = pg_connect("postgres://cjltmuraqwmwns:3f6f4747c60e84cedd897375b76942235894bfa34136d489fb78e68e3121ea50@ec2-3-222-235-188.compute-1.amazonaws.com:5432/d48s61rqnmsirk");
     if (!$Connect) {
         die("Connection failed");
-    }*/
+    }
 ?>
