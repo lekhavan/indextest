43df754b351a4fb3f8401d31db3336d56b30ef83
lekhavan <91652143+lekhavan@users.noreply.github.com>
2021-10-23 08:08:38 +0700

    .

---
+++ Shopping/connection.php
@@ -0,0 +1,7 @@
+<?php
+    $conn = mysqli_connect('localhost', 'root', '', '1111') or die("Can not connect database".mysqli_connect_error());
+    /*$Connect = pg_connect("postgres://kkhbsaqkzdqykx:54ddd61a4283d1770df62432ed7f08b0ac42ba7ae7d9e2bb519810225fff3af3@ec2-3-212-168-103.compute-1.amazonaws.com:5432/df5bjm5q08o5k2");
+    if (!$Connect) {
+        die("Connection failed");
+    }*/
+?>
